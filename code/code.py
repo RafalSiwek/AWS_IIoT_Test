63a2ac68255e9d136feb28bffc33dc404740e30c
RafalSiwek <rafal.siwek@o2.pl>
2021-01-13 19:47:03 +0100

    minor test changes

--- code/code.py
+++ code/code.py
@@ -1,7 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    print("Hello Lambda")
+    print("Hello Lambda"):
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
