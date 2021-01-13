efe11742331374259de894d3b8b180136566fef9
RafalSiwek <rafal.siwek@o2.pl>
2021-01-13 12:23:51 +0100

    Lambda test code

--- code/code.py
+++ code/code.py
@@ -1,3 +1,8 @@
-import paho.mqtt.client as mqtt
+import json
 
-print(f'bye')
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
