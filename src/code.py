831604674849cacc9b73a86eced8dc3697f945be
RafalSiwek <rafal.siwek@o2.pl>
2021-01-29 16:44:28 +0100

    minor changes

--- src/code.py
+++ src/code.py
@@ -4,12 +4,14 @@ from .eventmodels import S3Model
 from aws_lambda_powertools.utilities.parser import parse, event_parser, ValidationError
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
+print(f"Hello")
 
 def lambda_handler(event, context: LambdaContext) -> None:
     try:
         print("Hello Lambda")
         parse(event, model=S3Model)
     except ValidationError:
+        print(f'Success')
         return {
             'statusCode': 400,
             'body': json.dumps('Validation error'),
